d30bc8a379e5c5f22a8d45b783c4921a43949041
Tommy <tommyshiels@gmail.com>
2020-05-28 15:43:00 -0700

    added front and back end communication

--- chessts/game/views.py
+++ chessts/game/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import sys
+sys.path.append('/home/tommy/Code/chess/mychess')
 import json
-
+import alpha_beta as ab
+import chess
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -10,9 +12,12 @@ from django.views.decorators.csrf import csrf_exempt
 def play(request):
     #FIX ME: change getMove route to this to check if request.method == "POST"
     if (request.method == "POST"):
-        output = json.loads(request.body)
-        print(output['value'])
-        return HttpResponse("this is a test!!!")
+        fen = json.loads(request.body)
+        print(fen['value'])
+        board = chess.Board()
+        board.set_fen(fen['value'])
+        move, score = ab.minimax(board, 3, 1, -10000, 10000)
+        return HttpResponse(move.uci())
     return render(request, 'game/board.html')
 
 @csrf_exempt
